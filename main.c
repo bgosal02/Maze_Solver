d2f35a74a894d344cb19b40175c618a427409a37
mparhar1 <83943195+mparhar1@users.noreply.github.com>
2022-01-07 20:52:44 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ File:          main.c
+ Purpose:       Driver for Lab4 Takehome
+ Date:			November 11, 2021
+ */
+
+
+/* Preprocessor directives to include libraries */
+#include "mazesolver.h"
+
+/*
+Main function drives the program.
+PRE:       None
+POST:      None
+RETURN:    IF the program exits correctly
+            THEN 0 ELSE 1
+*/
+int main(void)
+{
+  /* Hand off work to process function */
+  process();
+
+  /* Ends main function */
+  system("pause");
+  return 0;
+}
